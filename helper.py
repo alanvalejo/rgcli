48db18d1f91e960ebd8b0a87521b4b8b4f0bcd05
Alan Valejo <alanvalejo@gmail.com>
2016-12-16 17:02:06 -0200

    update

--- helper.py
+++ helper.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+RGCLI (Robust Graph that Considers Labeled Instances)
+=====================================================
+
+Copyright (C) 2016 Alan Valejo <alanvalejo@gmail.com> All rights reserved
+Copyright (C) 2016 Thiago Faleiros <thiagodepaulo@gmail.com> All rights reserved
+
+To exploit the informativeness conveyed by these few labeled
+instances available in semi-supervised scenarios.
+
+This file is part of RGCLI.
+
+RGCLI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RGCLI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RGCLI. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+__maintainer__ = 'Alan Valejo'
+__author__ = 'Alan Valejo, Thiago Faleiros'
+__email__ = 'alanvalejo@gmail.com', 'thiagodepaulo@gmail.com'
+__credits__ = ['Alan Valejo', 'Thiago Faleiros', 'Lilian Berton', 'Jorge Valverde-Rebaza', 'Alneu de Andrade Lopes']
+__license__ = 'GNU'
+__docformat__ = 'restructuredtext en'
+__version__ = '0.1'
+__date__ = '2016-12-01'
+
 def write_ncol(output, edgelist):
 	with open(output, 'w') as f:
 		f.write(edgelist)
--- rgcli.py
+++ rgcli.py
@@ -5,10 +5,26 @@
 RGCLI (Robust Graph that Considers Labeled Instances)
 =====================================================
 
-To exploit the informativeness conveyed by these few labeled instances
-available in semi-supervised scenarios.
+Copyright (C) 2016 Alan Valejo <alanvalejo@gmail.com> All rights reserved
+Copyright (C) 2016 Thiago Faleiros <thiagodepaulo@gmail.com> All rights reserved
 
-Permission to copy and modify is granted under the foo license.
+To exploit the informativeness conveyed by these few labeled
+instances available in semi-supervised scenarios.
+
+This file is part of RGCLI.
+
+RGCLI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+RGCLI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RGCLI. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import csv
